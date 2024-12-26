c2d6371edd153fb6d5e6f3bbc539ab3fa59a217f
DAI <malias@mitaoe.ac.in>
2024-12-26 16:14:40 +0530

    version 2

--- kubernetes.py
+++ kubernetes.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'Welcome to Kubernetes!'
+	return 'I am bored... tomarrow I will hire jenkins'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=7000, debug=True)
